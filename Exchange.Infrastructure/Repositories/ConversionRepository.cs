ec007ab04f365ced48d49c39b2353b61b22b0990
tamtii <56804288+tamtii@users.noreply.github.com>
2025-09-14 14:30:22 +0400

    add IconversionRepository and interface

---
+++ Exchange.Infrastructure/Repositories/ConversionRepository.cs
@@ -0,0 +1,19 @@
+﻿using Exchange.Domain.Interfaces;
+using Exchange.Infrastructure.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exchange.Infrastructure.Repositories
+{
+    public class ConversionRepository : IConversionsRepository
+    {
+        private readonly ExchangeDbContext _db;
+        public ConversionRepository(ExchangeDbContext db)
+        {
+            _db = db;
+        }
+    }
+}
