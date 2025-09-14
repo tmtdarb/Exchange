93475af67f349204d401db3333211f474422acc0
tamtii <56804288+tamtii@users.noreply.github.com>
2025-09-14 14:43:42 +0400

    register

--- Exchange.Api/Program.cs
+++ Exchange.Api/Program.cs
@@ -1,4 +1,6 @@
+using Exchange.Domain.Interfaces;
 using Exchange.Infrastructure.EF;
+using Exchange.Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,6 +13,9 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<ExchangeDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<ICurrencyRepository,CurrencyRepository>();
+builder.Services.AddScoped<IExchangeRatesRepository, ExchangeRatesRepository>();
+builder.Services.AddScoped<IConversionsRepository, ConversionRepository>();
 
 var app = builder.Build();
 
