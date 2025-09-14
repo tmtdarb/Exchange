1de61696f9cf0834ee4be54bb50641c23a71d282
tamtii <56804288+tamtii@users.noreply.github.com>
2025-09-14 14:26:17 +0400

    exchangerates repo and interface

---
+++ Exchange.Domain/Interfaces/IExchangeRatesRepository.cs
@@ -0,0 +1,14 @@
+﻿using Exchange.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exchange.Domain.Interfaces
+{
+    public interface IExchangeRatesRepository
+    {
+        Task<List<ExchangeRate>> GetAllExchangeRates();
+    }
+}
---
+++ Exchange.Infrastructure/Repositories/ExchangeRatesRepository.cs
@@ -0,0 +1,26 @@
+﻿using Exchange.Domain.Entities;
+using Exchange.Domain.Interfaces;
+using Exchange.Infrastructure.EF;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exchange.Infrastructure.Repositories
+{
+    public class ExchangeRatesRepository : IExchangeRatesRepository
+    {
+        private readonly ExchangeDbContext _db;
+        public ExchangeRatesRepository(ExchangeDbContext db)
+        {
+            _db = db;
+        }
+        public async Task<List<ExchangeRate>> GetAllExchangeRates()
+        {
+            var exchangeRates = await _db.ExchangeRates.Include(a=>a.Currency).ToListAsync();
+            return exchangeRates;
+        }
+    }
+}
